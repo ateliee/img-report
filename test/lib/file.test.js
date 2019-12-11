b0cdd045f1721c93baf02ad81b510e37cc3a3752
ateliee <ateliee@gmail.com>
2019-12-12 01:58:30 +0900

    test case add

--- test/lib/file.test.js
+++ test/lib/file.test.js
@@ -3,6 +3,7 @@ var chai = require('chai')
     , should = chai.should();
 var assert = require('assert');
 var file = require('../../lib/file');
+const fs = require('fs');
 
 describe('Lib/file', function() {
     // describe('#indexOf()', function() {
@@ -14,6 +15,60 @@ describe('Lib/file', function() {
         it('files read check for examples/assets', function(done) {
             file._getDirImageList(__dirname+'/../../examples/assets').then((data) => {
                 expect(data).to.be.an('array').that.does.be.length(8)
+            }).then(done, done);
+        });
+    });
+    describe('getImageList()', function() {
+        it('files read check for examples/assets', function(done) {
+            file.getImageList(__dirname+'/../../examples/assets').then((data) => {
+                expect(data)
+                    .to.be.an('object')
+                    .to.have.property('example1')
+                    .that.does.be.length(4)
+            }).then(done, done);
+        });
+    });
+    let tmp_dir = __dirname+'/../../tmp';
+    let tmp_path = tmp_dir+'/sample.json';
+    function setupWriteFile(){
+        if (fs.existsSync(tmp_path)){
+            fs.unlinkSync(tmp_path);
+        }
+        if (!fs.existsSync(tmp_dir)){
+            fs.mkdirSync(tmp_dir);
+        }
+    }
+    before(setupWriteFile);
+    describe('_writeFile()', function() {
+        it('make file', function(done) {
+            let data = JSON.stringify({aaa: 'bbb'})
+            file._writeFile(tmp_path, data).then(() => {
+                expect(fs.existsSync(tmp_path))
+                    .to.be.eq(true)
+            }).then(done, done);
+        });
+        it('rm files', function(done) {
+            file.rmdirSync(tmp_dir).then(() => {
+                expect(fs.existsSync(tmp_dir))
+                    .to.be.eq(false)
+            }).then(done, done);
+        });
+        it('make file and dir2', function(done) {
+            let data = JSON.stringify({aaa: 'bbb'})
+            file._writeFile(tmp_path, data).then(() => {
+                expect(fs.existsSync(tmp_path))
+                    .to.be.eq(true)
+            }).then(done, done);
+        });
+    });
+    describe('genearteDiffImages()', function() {
+        this.timeout(10000);
+        it('generate diff report from examples/assets', function(done) {
+            let src = __dirname+'/../../examples/assets';
+            let dist = __dirname+'/../../tmp/dist';
+            file.genearteDiffImages(src, dist, true).then((data) => {
+                expect(fs.existsSync(dist+'/report.json'))
+                    .to.be.eq(true)
             }).then(done, done);
         });
     });
